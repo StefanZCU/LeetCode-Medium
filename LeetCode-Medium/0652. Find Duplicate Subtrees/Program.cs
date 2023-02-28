efb98383615cd8d19bca252d9bc380feca6463bd
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-28 12:02:49 +0200

    Create Program.cs

---
+++ LeetCode-Medium/0652. Find Duplicate Subtrees/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _0652._Find_Duplicate_Subtrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
