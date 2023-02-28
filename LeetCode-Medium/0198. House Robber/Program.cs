58391d637acd118caf69fb511cb15f98c1f27896
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-28 11:58:05 +0200

    Create Program.cs

---
+++ LeetCode-Medium/0198. House Robber/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        int highestSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            
+        }
+
+        return highestSum;
+    }
+}
