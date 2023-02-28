53e5a845e8b2d297dd17859e0b16507570fe726a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-28 12:13:00 +0200

    Create Program.cs

---
+++ LeetCode-Medium/0003. Longest Substring Without Repeating Characters/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+
+        int longestSubstring = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+            if (!dict.ContainsKey(c))
+            {
+                dict.Add(c, i);
+                longestSubstring = Math.Max(dict.Count, longestSubstring);
+            }
+            else
+            {
+                i = dict[c];
+                dict.Clear();
+            }
+        }
+        return longestSubstring;
+    }
+}
