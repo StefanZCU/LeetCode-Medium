70f3d436090279070521662a49e4874e2bce66ad
Stefan Zhivkov <stefan.r.zhivkov@gmail.com>
2023-11-19 16:28:08 +0200

    Added 1910. Remove All Occurrences of a Substring

---
+++ LeetCode-Medium/1910. Remove All Occurrences of a Substring/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _1910._Remove_All_Occurrences_of_a_Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveOccurrences("daabcbaabcbc", "abc"));
+        }
+
+
+        public static string RemoveOccurrences(string s, string part)
+        {
+
+            int index;
+            while ((index = s.IndexOf(part)) != -1)
+            {
+                s = s.Remove(index, part.Length);
+            }
+            return s;
+
+        }
+    }
+}
